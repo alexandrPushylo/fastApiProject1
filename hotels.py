38a6011c6ab2e87cad5b844f3fae49fdf1f568a7
Alexandr Pushylo <alexandr.pushylo@gmail.com>
2025-09-16 20:01:36 +0300

    add new hotels

--- hotels.py
+++ hotels.py
@@ -4,18 +4,16 @@ from schemas.hotels import Hotel, HotelPATCH
 
 router = APIRouter(prefix="/hotels", tags=["Отели"])
 
-
 hotels = [
-    {
-        "id": 1,
-        "title": "Hotel 1",
-        "name": "Hotel 1",
-    },
-    {
-        "id": 2,
-        "title": "Hotel 2",
-        "name": "Hotel 2",
-    },
+    {"id": 1, "title": "Hotel 1", "name": "Hotel 1"},
+    {"id": 2, "title": "Hotel 2", "name": "Hotel 2"},
+    {"id": 3, "title": "Sochi", "name": "sochi"},
+    {"id": 4, "title": "Дубай", "name": "dubai"},
+    {"id": 5, "title": "Мальдивы", "name": "maldivi"},
+    {"id": 6, "title": "Геленджик", "name": "gelendzhik"},
+    {"id": 7, "title": "Москва", "name": "moscow"},
+    {"id": 8, "title": "Казань", "name": "kazan"},
+    {"id": 9, "title": "Санкт-Петербург", "name": "spb"},
 ]
 
 
