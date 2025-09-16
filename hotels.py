20cb2cecdac7e9b31bb90cdd21f85298a7a1204c
Alexandr Pushylo <alexandr.pushylo@gmail.com>
2025-09-16 19:51:12 +0300

    create hotel schema

--- hotels.py
+++ hotels.py
@@ -1,21 +1,8 @@
-from fastapi import Query, Body, APIRouter
+from fastapi import Query, APIRouter
 
+from schemas.hotels import Hotel, HotelPATCH
 
 router = APIRouter(prefix="/hotels", tags=["Отели"])
-
-
-hotels = [
-    {
-        "id": 1,
-        "title": "Hotel 1",
-        "name": "Hotel 1",
-    },
-    {
-        "id": 2,
-        "title": "Hotel 2",
-        "name": "Hotel 2",
-    },
-]
 
 
 @router.get("", summary="Получить список отелей")
@@ -38,13 +25,12 @@ def get_hotels(
 
 @router.post("", summary="Создать отель")
 def create_hotel(
-        name: str = Body(),
-        title: str = Body(),
+        data: Hotel
 ):
     hotels.append({
         "id": len(hotels) + 1,
-        "name": name,
-        "title": title,
+        "name": data.name,
+        "title": data.title,
     })
     return {"success": True}
 
@@ -62,14 +48,13 @@ def delete_hotel(
 @router.put("/{hotel_id}", summary="Обновить отель")
 def update_hotel(
         hotel_id: int,
-        name: str = Body(),
-        title: str = Body(),
+        data: Hotel,
 ):
     global hotels
     for hotel in hotels:
         if hotel['id'] == hotel_id:
-            hotel['name'] = name
-            hotel['title'] = title
+            hotel['name'] = data.name
+            hotel['title'] = data.title
 
     return {"success": True}
 
@@ -77,15 +62,14 @@ def update_hotel(
 @router.patch("/{hotel_id}", summary="Частично обновить отель")
 def patch_hotel(
         hotel_id: int,
-        name: str | None = Body(default=None),
-        title: str | None = Body(default=None),
+        data: HotelPATCH
 ):
     global hotels
     for hotel in hotels:
         if hotel['id'] == hotel_id:
-            if name:
-                hotel['name'] = name
-            if title:
-                hotel['title'] = title
+            if data.name:
+                hotel['name'] = data.name
+            if data.title:
+                hotel['title'] = data.title
 
     return {"success": True}
