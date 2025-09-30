ed21a989acd5819a4968706e433794bc2e8d2ec1
Alexandr Pushylo <alexandr.pushylo@gmail.com>
2025-09-30 20:58:27 +0300

    ref

--- src/main.py
+++ src/main.py
@@ -7,7 +7,6 @@ from pathlib import Path
 sys.path.append(str(Path(__file__).parent.parent))
 
 from src.api.hotels import router as hotels_router
-from src.config import settings
 
 
 app = FastAPI()
