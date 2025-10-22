ca6962ca4d566e6f418ca13fb3840e0ef372a64f
Alexandr Pushylo <alexandr.pushylo@gmail.com>
2025-10-22 19:49:51 +0300

    change to unique field

---
+++ src/migrations/versions/2025_10_22_1946-39849deade10_edit_user_email.py
@@ -0,0 +1,29 @@
+"""edit user.email
+
+Revision ID: 39849deade10
+Revises: e130ab42603c
+Create Date: 2025-10-22 19:46:23.796064
+
+"""
+
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = "39849deade10"
+down_revision: Union[str, Sequence[str], None] = "e130ab42603c"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    op.create_unique_constraint(None, "users", ["email"])
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    op.drop_constraint(None, "users", type_="unique")
