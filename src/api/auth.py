d1e70d4f1663daffbbfa223111231352c25167f3
Alexandr Pushylo <alexandr.pushylo@gmail.com>
2025-10-22 19:25:37 +0300

    add POST /auth/register; hashed_password not ready yet

---
+++ src/api/auth.py
@@ -0,0 +1,21 @@
+from fastapi import Query, APIRouter, Body, HTTPException
+
+
+from src.database import async_session_maker
+from src.repositories.users import UsersRepository
+from src.schemas.users import UserRequestAdd, UserAdd
+
+router = APIRouter(prefix="/auth", tags=["Аутентификация и авторизация"])
+
+
+@router.post("/register", summary="Регистрация пользователя")
+async def register_user(
+        data: UserRequestAdd
+):
+    hashed_password = data.password
+    new_user_data = UserAdd(email=data.email, hashed_password=hashed_password)
+    async with async_session_maker() as session:
+        await UsersRepository(session).add(new_user_data)
+        await session.commit()
+
+    return {'status': 'ok'}
--- src/main.py
+++ src/main.py
@@ -6,13 +6,13 @@ from pathlib import Path
 
 sys.path.append(str(Path(__file__).parent.parent))
 
+from src.api.auth import router as auth_router
 from src.api.hotels import router as hotels_router
 
 
 app = FastAPI()
-app.include_router(
-    hotels_router,
-)
+app.include_router(auth_router)
+app.include_router(hotels_router)
 
 
 if __name__ == '__main__':
