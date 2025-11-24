3929b92e52ddecb6d349a48c59913e2fb2f796b7
Alexandr Pushylo <alexandr.pushylo@gmail.com>
2025-11-24 20:55:57 +0300

    fix: get_filtered_by_time

--- src/repositories/hotels.py
+++ src/repositories/hotels.py
@@ -21,7 +21,7 @@ class HotelsRepository(BaseRepository):
             title,
             limit,
             offset
-    ):
+    ) -> list[Hotel]:
         rooms_ids_to_get = rooms_ids_for_booking(date_from=date_from, date_to=date_to)
 
         hotels_ids_to_get = (
@@ -29,26 +29,28 @@ class HotelsRepository(BaseRepository):
             .select_from(RoomsOrm)
             .filter(RoomsOrm.id.in_(rooms_ids_to_get))
         )
+        query = select(HotelsOrm).filter(HotelsOrm.id.in_(hotels_ids_to_get))
 
         if location:
-            hotels_ids_to_get = (
-                hotels_ids_to_get
+            query = (
+                query
                 .filter(
                     func.lower(self.model.location).contains(location.strip().lower())
                 )
             )
 
         if title:
-            hotels_ids_to_get = (
-                hotels_ids_to_get
+            query = (
+                query
                 .filter(func.lower(HotelsOrm.title).contains(title.strip().lower()))
             )
 
-        hotels_ids_to_get = (
-            hotels_ids_to_get
+        query = (
+            query
             .limit(limit)
             .offset(offset)
         )
+        result = await self.session.execute(query)
 
-        return await self.get_filtered(HotelsOrm.id.in_(hotels_ids_to_get))
+        return [Hotel.model_validate(hotel, from_attributes=True) for hotel in result.scalars().all()]
 
