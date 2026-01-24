20d25ef316769caf32a7a7191485e4aed7f6002e
Alexandr Pushylo <alexandr.pushylo@gmail.com>
2026-01-24 22:18:37 +0300

    add check_test_mode fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,9 +7,12 @@ from src.config import settings
 
 
 @pytest.fixture(scope="session", autouse=True)
-async def async_main():
+async def check_test_mode():
     assert settings.MODE == "TEST"
 
+
+@pytest.fixture(scope="session", autouse=True)
+async def async_main(check_test_mode):
     async with engine_null_pool.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
